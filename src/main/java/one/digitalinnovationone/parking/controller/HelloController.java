f76beb823005276c39079e5af1fe49f543259f74
rodolfo-luna <rodolfotff@gmail.com>
2023-09-04 12:00:07 -0300

    adicionando segurança e swagger

--- src/main/java/one/digitalinnovationone/parking/controller/HelloController.java
+++ src/main/java/one/digitalinnovationone/parking/controller/HelloController.java
@@ -1,14 +1,19 @@
-package one.digitalinnovationone.parking.controller;
+package one.digitalinnovation.parking.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import springfox.documentation.annotations.ApiIgnore;
+
 @RestController
 @RequestMapping("/")
+@ApiIgnore
 public class HelloController {
+
     @GetMapping
-    public String hello() {
-        return "Hello DIO...";
+    public String hello(){
+        return "Hello DIO. Java devs";
     }
+
 }
